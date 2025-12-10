b92d35b2a956e309af5d7f7cc9f24bae0b200223
Domenico Calautti <domeniccalautti@gmail.com>
2025-12-10 18:16:41 -0500

    fix vite crash

--- Frontend/src/api/api.jsx
+++ Frontend/src/api/api.jsx
@@ -1,4 +1,7 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL + "/api";
-const API_ADMIN_BASE_URL = import.meta.env.VITE_API_BASE_URL + "/api/admin";
+const BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:2424";
 
+const API_BASE_URL = `${BASE}/api`;
+const API_ADMIN_BASE_URL = `${BASE}/api/admin`;
+
+export default API_BASE_URL;
 export { API_BASE_URL, API_ADMIN_BASE_URL };
